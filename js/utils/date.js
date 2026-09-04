d15ad6b5db718cee05710788506ad0ec4719da35
João Pedro Pinheiro <joaoppinheiro632@gmail.com>
2026-09-04 09:01:43 -0300

    Delete js directory

--- js/utils/date.js
+++
@@ -1,8 +0,0 @@
-export function parseDate(dateStr) {
-    if (!dateStr) return null;
-    const parts = dateStr.split('T')[0].split('-');
-    if (parts.length === 3) {
-        return new Date(parts[0], parts[1] - 1, parts[2]);
-    }
-    return null;
-}
